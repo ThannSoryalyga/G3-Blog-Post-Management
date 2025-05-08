bb7a04da106ecf91472c0bd4509d03dd700d2576
Sokhon Senghong <sokhon.senghong@institute.pse.ngo>
2025-05-08 13:44:14 +0700

    add login and register on header because forget to add it at menu

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import Header from "./components/header";
 import About from "./pages/about";
 import Blogs from "./pages/blogs";
 import Contact from "./pages/contact";
-// import Home from "./pages/home";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
 
 const App = () => {
   return (
@@ -11,10 +13,12 @@ const App = () => {
       <Header />
       <div className="container mx-auto mt-8">
         <Routes>
-          {/* <Route path="/home" element={<Home />} /> */}
+          <Route path="/home" element={<Home />} />
           <Route path="/blogs" element={<Blogs />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </div>
     </Router>
